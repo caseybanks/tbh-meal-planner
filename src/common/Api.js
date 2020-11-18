9039d91bb0dbfde5ceb4cefdc446072fe009fa0b
Casey Banks <casey.banks@theoaklandgroup.co.uk>
2020-11-18 21:07:41 +0000

    add: Api.js Contains all the API URLs

---
+++ src/common/Api.js
@@ -0,0 +1,8 @@
+export const API = {
+  API_SEARCH: "https://www.themealdb.com/api/json/v1/1/search.php",
+  API_LOOKUP: "https://www.themealdb.com/api/json/v1/1/lookup.php",
+  API_RANDOM: "https://www.themealdb.com/api/json/v1/1/random.php",
+  API_CATEGORIES: "https://www.themealdb.com/api/json/v1/1/categories.php",
+  API_LIST: "https://www.themealdb.com/api/json/v1/1/list.php",
+  API_FILTER: "https://www.themealdb.com/api/json/v1/1/filter.php",
+};
