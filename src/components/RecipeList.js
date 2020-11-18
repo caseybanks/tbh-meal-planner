76baefcc338644f9b05cda88162b6b94188262b1
Casey Banks <casey.banks@theoaklandgroup.co.uk>
2020-11-18 21:15:13 +0000

    add: RecipeList.js component to getRecipies() & map results

---
+++ src/components/RecipeList.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { Recipe } from "./Recipe";
+import { Filter } from "./Filter";
+import { API } from "../common/Api";
+
+export function RecipeList() {
+  const [filter, setFilter] = useState("");
+  const [recipies, setRecipies] = useState([]);
+
+  const getRecipies = async () => {
+    try {
+      const res = await fetch(API.API_SEARCH + "?s=" + filter);
+      const recipies = await res.json();
+      setRecipies(recipies.meals);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getRecipies();
+  }, []);
+
+  return (
+    <div>
+      <Filter filter={filter} setFilter={setFilter} />
+      <ul>
+        {recipies.filter((recipe) =>
+                    recipe.strMeal.toLowerCase().includes(filter.toLowerCase())
+                ).map((recipe) => (
+            <Recipe key={recipe.idMeal} recipe={recipe} />
+          ))}
+      </ul>
+    </div>
+  );
+}
