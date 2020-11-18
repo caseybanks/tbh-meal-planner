60ed45f5f2adf36b296c656c938c179dd6cbd627
Casey Banks <casey.banks@theoaklandgroup.co.uk>
2020-11-18 21:06:58 +0000

    fix: Reformated Menu.js

--- src/common/Menu.js
+++ src/common/Menu.js
@@ -1,23 +1,22 @@
 export const MENU = {
-    menuItems: [
-      {
-        id: 1,
-        label: "Week Planner",
-        href: "/",
-        eventKey: "",
-      },
-      {
-        id: 2,
-        label: "How to Use",
-        href: "howtouse",
-        eventKey: "howtouse",
-      },
-      {
-        id: 3,
-        label: "Contact",
-        href: "contact",
-        eventKey: "contact",
-      },
-    ],
-  };
-  
+  menuItems: [
+    {
+      id: 1,
+      label: "Week Planner",
+      href: "/",
+      eventKey: "",
+    },
+    {
+      id: 2,
+      label: "How to Use",
+      href: "howtouse",
+      eventKey: "howtouse",
+    },
+    {
+      id: 3,
+      label: "Contact",
+      href: "contact",
+      eventKey: "contact",
+    },
+  ],
+};
