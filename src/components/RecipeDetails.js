070aaff245b56eb20517023d22b75526d2b7de15
Casey Banks <casey.banks@theoaklandgroup.co.uk>
2020-11-18 21:18:04 +0000

    add: RecipeDetails.js to getRecipe() by id & map to page

---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,37 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { API } from "../common/Api";
+
+export function RecipeDetails() {
+  const { idMeal } = useParams();
+  const [recipe, setRecipe] = useState({});
+
+  const getRecipe = async () => {
+    try {
+      const res = await fetch(API.API_LOOKUP + "?i=" + idMeal);
+      const recipe = await res.json();
+      setRecipe(recipe.meals);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getRecipe();
+  }, [idMeal]);
+
+  if (!recipe.strMeal) return null;
+
+  return (
+    <div>
+      <img src={recipe.strMealThumb} alt={recipe.strMeal + " recipe"} />
+      <div>
+        <h2>{recipe.strMeal}</h2>
+        <p>{recipe.strCategory}</p>
+        <p>{recipe.strArea}</p>
+        <p>{recipe.strInstructions}</p>
+        <p>{recipe.strCategory}</p>
+      </div>
+    </div>
+  );
+}
