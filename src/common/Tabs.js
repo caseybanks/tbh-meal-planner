9a0b68e1660dc1a19ba1721697869f6e0c6dc3ab
Casey Banks <casey.banks@theoaklandgroup.co.uk>
2020-11-18 17:32:50 +0000

    add: Tab naviation to week planner page

---
+++ src/common/Tabs.js
@@ -0,0 +1,49 @@
+export const TABS = {
+  tabs: [
+    {
+      id: 1,
+      label: "Planner",
+      eventKey: "planner",
+    },
+    {
+      id: 2,
+      label: "Monday",
+      eventKey: "monday",
+    },
+    {
+      id: 3,
+      label: "Tuesday",
+      eventKey: "tuesday",
+    },
+    {
+      id: 4,
+      label: "Wednesday",
+      eventKey: "wednesday",
+    },
+    {
+      id: 5,
+      label: "Thursday",
+      eventKey: "thursday",
+    },
+    {
+      id: 6,
+      label: "Friday",
+      eventKey: "friday",
+    },
+    {
+      id: 7,
+      label: "Saturday",
+      eventKey: "saturday",
+    },
+    {
+      id: 8,
+      label: "Sunday",
+      eventKey: "sunday",
+    },
+    {
+      id: 9,
+      label: "Shopping List",
+      eventKey: "",
+    },
+  ],
+};
---
+++ src/components/TabNavigation.js
@@ -0,0 +1,21 @@
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { TABS } from "../common/Tabs";
+
+export function TabNavigation() {
+  const tabs = Object.values(TABS["tabs"]);
+  console.log(tabs);
+
+  return (
+    <Container fluid>
+      <Nav variant="tabs" defaultActiveKey="planner">
+        {tabs.map((tab) => (
+          <Nav.Item key={tab.id}>
+            <Nav.Link eventKey={tab.eventKey}>{tab.label}</Nav.Link>
+          </Nav.Item>
+        ))}
+      </Nav>
+    </Container>
+  );
+}
