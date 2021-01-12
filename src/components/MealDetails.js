133f3e762b36dfb9f4612cf3a6f910f2ab5ac924
Casey Banks <casey.banks@theoaklandgroup.co.uk>
2021-01-12 21:26:13 +0000

    fix: Significant refactor of recipe search results
    * Deletion of Recipe.js and removal of modal with Recipe details listed
    * All the code from Recipe.js and RecipeList.js moved into Search.js and
      MealDetails.js
    * Deletion of RecipeList.js
    * Implemented savedPlannerMeals useState as a parent component to
      Search.js so props.savedPlannerMeals can be used within the child
      components

--- src/components/MealDetails.js
+++ src/components/MealDetails.js
@@ -8,84 +8,28 @@ export function MealDetails(props) {
 
     const activeTab = props.tab.key;
     const [show, setShow] = useState(false);
-    const [storedMeal, setStoredMeal] = useState({
-      mealEvent: "",
-      weekday: "",
-      recipe: {
-        idMeal: "",
-        strMeal: "",
-        strDrinkAlternate: "",
-        strCategory: "",
-        strArea: "",
-        strInstructions: "",
-        strMealThumb: "",
-        strTags: "",
-        strYoutube: "",
-        strIngredient1: "",
-        strIngredient2: "",
-        strIngredient3: "",
-        strIngredient4: "",
-        strIngredient5: "",
-        strIngredient6: "",
-        strIngredient7: "",
-        strIngredient8: "",
-        strIngredient9: "",
-        strIngredient10: "",
-        strIngredient11: "",
-        strIngredient12: "",
-        strIngredient13: "",
-        strIngredient14: "",
-        strIngredient15: "",
-        strIngredient16: "",
-        strIngredient17: "",
-        strIngredient18: "",
-        strIngredient19: "",
-        strIngredient20: "",
-        strMeasure1: "",
-        strMeasure2: "",
-        strMeasure3: "",
-        strMeasure4: "",
-        strMeasure5: "",
-        strMeasure6: "",
-        strMeasure7: "",
-        strMeasure8: "",
-        strMeasure9: "",
-        strMeasure10: "",
-        strMeasure11: "",
-        strMeasure12: "",
-        strMeasure13: "",
-        strMeasure14: "",
-        strMeasure15: "",
-        strMeasure16: "",
-        strMeasure17: "",
-        strMeasure18: "",
-        strMeasure19: "",
-        strMeasure20: "",
-        strSource: "",
-        dateModified: "",
-      },
-    });
+    const [savedPlannerMeals, setSavedPlannerMeals] = useState([]);
     const [weekday, setWeekday] = useState('');
     const [mealEvent, setMealEvent] = useState('');
     const [recipe, setRecipe] = useState({});
 
-    const getStoredMeals = () => {
+    const getSavedPlannerMeals = () => {
         try {
-            const storedData = JSON.parse(localStorage.getItem('storedMeal'));
-            setStoredMeal(storedData);
-            setWeekday(storedData['weekday']);
-            setMealEvent(storedData['mealEvent']);
-            setRecipe(storedData['recipe']);
+            const mealsData = JSON.parse(localStorage.getItem('storedMeal'));
+            console.log('getMealsData',mealsData );
+            setSavedPlannerMeals(mealsData);
+            setWeekday(mealsData['weekday']);
+            setMealEvent(mealsData['mealEvent']);
+            setRecipe(mealsData['recipe']);
         } catch(err) {
             console.error(err);
-        };        
+        };
     };
-
+    
     useEffect(
-        () => {
-            getStoredMeals();   
-        },
-        []
+    () => {
+        getSavedPlannerMeals();
+    }, [props.tab]
     );
 
     return (
@@ -104,10 +48,10 @@ export function MealDetails(props) {
                     aria-labelledby="example-custom-modal-styling-title"
                 >
                     <Modal.Header closeButton>
-                        <h2>Search</h2>                        
+                        <h2>Search for a Recipe</h2>                        
                     </Modal.Header>
                     <Modal.Body>
-                        <Search/>                        
+                        <Search savedPlannerMeals={savedPlannerMeals} />
                     </Modal.Body>
                 </Modal>
             </div>
@@ -131,4 +75,4 @@ export function MealDetails(props) {
             </div>
         </div>
     );
-}
+};
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,15 +3,25 @@ import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { API } from '../common/Api';
-import { RecipeList } from './RecipeList';
+import { RecipeCard } from './RecipeCard';
 
 
-export function Search() {
+export function Search(props) {
 
   const [searchString, setSearchString] = useState("");
   const [recipes, setRecipes] = useState([]);
   const [resultsMessage, setResultsMessage] = useState("");
+  const [isSearchHidden, setIsSearchHidden] = useState(false);
+  const [isRecipeButtonHidden, setIsRecipeButtonHidden] = useState(false);
+  const [hideRecipeDetails, setHideRecipeDetails] = useState(true);
+  const mealTypeOptions = ['breakfast', 'lunch','dinner'].map(m => {return {key: m, value: m, text: m}});
+  const weekdayOptions = ['monday','tuesday','wednesday','thursday', 'friday','saturday','sunday'].map(m => {return {key: m, value: m, text: m}});
+  const [selectedMealEvent, setSelectedMealEvent] = useState('breakfast');
+  const [selectedWeekday, setSelectedWeekday] = useState('monday');
+  const [selectedRecipe, setSelectedRecipe] = useState({});
+  const [storedMeal, setStoredMeal] = useState([]);
 
+  //API call to Get recipes from https://www.themealdb.com
   const getRecipies = () => {
     fetch(API.API_SEARCH + '?s=' + searchString)
       .then((resp) => resp.json())
@@ -22,26 +32,67 @@ export function Search() {
       .catch((err) => console.error(err));
   };
 
+  //Search form submit
   const onSubmitSeach = (e) => {
     e.preventDefault();
     getRecipies(searchString);
+    setIsRecipeButtonHidden(false);
     if(recipes){
-      return setResultsMessage(<h5>Results for "{searchString}"</h5>)
+      return setResultsMessage(<div><h5>Results for "{searchString}"</h5></div>);
     } else {
       return setResultsMessage('');
-    }
-  };  
+    };
+  };
 
-  const onResetSearch = (e) => {
+  //Search form reset
+  const onResetSearch = (e) => {    
     e.preventDefault();
+    setIsSearchHidden(false);
+    setHideRecipeDetails(true);
     setSearchString('');
     setRecipes([]);
     setResultsMessage('');
   };
 
+  //Set search term for search form
+  const handleChange = (e) => {
+    if(e.currentTarget.id==='mealEvent') setSelectedMealEvent(e.currentTarget.value);
+    else if(e.currentTarget.id==='weekday') setSelectedWeekday(e.currentTarget.value);
+  };
+
+  //Handle selected recipe from search results
+  const handleSelect = (e) => { 
+    setHideRecipeDetails(false);
+    setIsRecipeButtonHidden(true);
+    setIsSearchHidden(true);
+    setHideRecipeDetails(false);
+
+  };
+
+  //Set selected mealEvent and weekday options with selectedRecipe
+  const handleSubmit = (e) => {
+    const newMealData = {};
+    const keys = ['weekday', 'mealEvent', 'recipe'];
+    const values = [selectedWeekday, selectedMealEvent, selectedRecipe];
+    
+    for (let i = 0; i < keys.length; i++) {
+      newMealData[keys[i]] = values[i];
+    };
+
+    const mealData = [...props.savedPlannerMeals].concat(newMealData);
+    console.log('newMealData: ', mealData);
+    setStoredMeal(mealData);
+  };
+
+  //Will store a list of meals data every time the value of storedMeal changes
+  useEffect(() => {
+    localStorage.setItem('storedMeal', JSON.stringify(storedMeal));
+  }, [storedMeal]);
+
   return (
     <div>
       <Form
+        hidden={isSearchHidden}
         inline
         onSubmit={onSubmitSeach}
         onReset={onResetSearch}
@@ -61,10 +112,59 @@ export function Search() {
         <Button variant="danger" type="reset">
           Clear
         </Button>
+        <div><p>Please enter at least one letter into the search box to return a recipe</p></div>
+        {resultsMessage}
       </Form>
-      <p>Please enter at least one letter into the search box to return a recipe</p>
-      {resultsMessage}
-      {(recipes) ? <RecipeList recipes={recipes} />:<h5>There are no results for '{searchString}', please try a different search term.</h5>} 
+
+      {(recipes) ? 
+        (
+          <div className="row" hidden={isRecipeButtonHidden}>
+            {recipes.map((recipe) => (
+              <Button key={recipe.idMeal} onClick={handleSelect} onClickCapture={()=>{setSelectedRecipe(recipe)}}>
+                <img
+                  src={recipe.strMealThumb + "/preview"}
+                  alt={recipe.strMeal + " thumbnail"}
+                />
+                <p>{recipe.strMeal}</p>
+              </Button>
+            ))}
+          </div>
+        ):<h5>There are no results for '{searchString}', please try a different search term.</h5>}      
+
+      <div hidden={hideRecipeDetails}>
+        <Button variant="danger" onClick={onResetSearch}>
+              Search again
+        </Button>
+        <Form inline className="justify-content-center" onSubmit={handleSubmit} >
+          <Form.Control
+            as="select"
+            className="my-1 mr-sm-2"
+            id="weekday"
+            custom
+            value={weekdayOptions.value}
+            onChange={handleChange}
+            placeholder="Select day of the week"
+            name="weekday"
+          >
+            {weekdayOptions.map((d) => (<option key={d.key} value={d.value}>{d.text}</option>))}
+          </Form.Control>
+          <Form.Control
+            as="select"
+            className="my-1 mr-sm-2"
+            id="mealEvent"
+            custom
+            value={mealTypeOptions.value}
+            onChange={handleChange}
+            placeholder="Select meal event"
+            name="mealEvent"
+          >
+            {mealTypeOptions.map((m) => (<option key={m.key} value={m.value}>{m.text}</option>
+            ))}
+          </Form.Control>
+          <Button type="submit" className="my-1">Select</Button>
+        </Form>
+        <RecipeCard recipe={selectedRecipe}/>
+        </div>
     </div>
   );
 };
